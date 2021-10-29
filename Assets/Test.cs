8215e530e39c50bc1a2d49472179de8d5a7adfa1
rui-miura <91726861+rui-miura@users.noreply.github.com>
2021-10-29 21:39:38 +0900

    ScriptTest
    ・要素が五個のint型の配列array
    順番に表示と逆に表示
    ・Bossクラス作成

--- Assets/Test.cs
+++ Assets/Test.cs
@@ -1,6 +1,33 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+public class Boss
+{
+    private int hp = 100;          // 体力
+    private int power = 25; // 攻撃力
+    private int mp = 53;
+    // 攻撃用の関数
+    public void Attack()
+    {
+        Debug.Log(this.power + "のダメージを与えた");
+    }
+
+    // 防御用の関数
+    public void Defence(int damage)
+    {
+        Debug.Log(damage + "のダメージを受けた");
+        // 残りhpを減らす
+        this.hp -= damage;
+    }
+    public void majic()
+    {
+        if (mp >= 5)
+        {   mp = mp - 5; }
+        Debug.Log("魔法攻撃をした。残りmpは" + mp);
+        if (mp < 5)
+        Debug.Log("mpが足りないため、魔法攻撃が使えない。");
+    }
+}
 
 public class Test : MonoBehaviour
 { 
@@ -28,6 +55,18 @@ public class Test : MonoBehaviour
         {
             Debug.Log(array[i]);
         }
+        Boss lastboss = new Boss();
+
+        // 攻撃用の関数を呼び出す
+        lastboss.Attack();
+        // 防御用の関数を呼び出す
+        lastboss.Defence(3);
+        for (int i = 0; i < 12; i++) 
+        { lastboss.majic(); }
+
+        
+            
+                    
     }
 
         
